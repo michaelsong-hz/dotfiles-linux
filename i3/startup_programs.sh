599c90ea0936770fc9aeedebae8c6255a1b9eb26
Michael Song <songhaozhou@gmail.com>
2016-01-28 13:40:58 -0500

    Resolved #1

---
+++ i3/startup_programs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Simple autostart file for i3-wm, executed from i3 config
+# I had to add this line to .bashrc:
+# export PATH=$PATH:/home/michael/.config/i3
+
+# Starts wicd-client on workspace 10
+i3-msg "workspace 10; exec wicd-client"
+
+# Starts FB messenger on workspace 2
+i3-msg "workspace 2; exec messengerfordesktop"
+
+exit 0
