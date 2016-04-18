c032bebd468dffbe952cab1657d4f9d7b2840ba0
Michael Song <songhaozhou@gmail.com>
2016-04-18 00:48:06 -0400

    Removed unnecessary components from check volume

--- lemonbar/check_volume.pl
+++ lemonbar/check_volume.pl
@@ -3,12 +3,7 @@
 
 ## The sink we are interested in should be given as the 
 ## 1st argument to the script.
-die("Need a sink number as the first argument\n") if @ARGV < 1;
 my $sink=$ARGV[0];
-
-## If the script has been run with a second argument,
-## that argument will be the volume threshold we are checking
-my $volume_limit=$ARGV[1]||undef;
 
 ## Run the pactl command and save the output in 
 ## the filehandle $fh
@@ -25,7 +20,7 @@ while (<$fh>) {
         ## Checks if it is muted
         /Mute: (.+?)/;
         if ($1 eq 'y') { print "M("; }
-        ## Extract the current colume of this sink
+        ## Extract the current volume of this sink
         /Volume:.*?(\d+)%/;
         print "$1\n";
     }
