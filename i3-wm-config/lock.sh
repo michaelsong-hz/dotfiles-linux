2537e5362565f75a91637cd937f1d35da3f4183f
Michael Song <michael.song.hz@gmail.com>
2017-06-15 23:17:34 -0400

    Switched to polybar

--- i3-wm-config/lock.sh
+++ i3-wm-config/lock.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-scrot /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
-i3lock -i /tmp/screenshotblur.png
+image_file=/tmp/screen_lock.png
+resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
+filters='noise=alls=10,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor'
+ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i $DISPLAY -vframes 1 \
+  -vf "$filters" "$image_file"
+i3lock -e -i "$image_file"
--- i3-wm-config/set_dpi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Simple autostart file for i3-wm, executed from i3 config
-
-# Sets DPI to 150%
-xrandr --dpi 144
-i3-msg "restart"
